99263f1fea1bb50ea2ff3de6dfb84d9951960a03
suyu06 <suyu.zhang@johnabbottcollege.net>
2023-01-26 21:04:57 -0500

    handling data using react state

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 //import logo from './logo.svg';
+import React, { useState } from "react";
 import "./App.css";
 //import TodoRowItem from "./components/TodoRowItem"
 import TodoTable from "./components/TodoTable";
 
 function App() {
-  const todos = [
-    {rowNumber:1, rowDescription:'Feed puppy',rowAssigned:'User one'},
-    {rowNumber:2, rowDescription: 'Water plants',rowAssigned:'User Two'},
-    {rowNumber:3, rowDescription: 'Make dinner',rowAssigned:'User one'},
-    {rowNumber:4, rowDescription: 'charge phone battery',rowAssigned:'User one'},
-]
+  const [todos, setTodos] = useState([
+    { rowNumber: 1, rowDescription: "Feed puppy", rowAssigned: "User one" },
+    {
+      rowNumber: 2,
+      rowDescription: "Water plants setTodos",
+      rowAssigned: "User Two",
+    },
+    { rowNumber: 3, rowDescription: "Make dinner", rowAssigned: "User one" },
+    {
+      rowNumber: 4,
+      rowDescription: "charge phone battery",
+      rowAssigned: "User one",
+    },
+  ]);
 
   const addTOdo = () => {
-    if(todos.length > 0){
+    if (todos.length > 0) {
       const newTodo = {
-        rowNumber:todos.length+1,
-        rowDescription:'New Todo',
-        rowAssigned:'User Three'
-      }
-      todos.push(newTodo);
-      console.log(todos);
+        rowNumber: todos.length + 1,
+        rowDescription: "New Todo",
+        rowAssigned: "User Three",
+      };
+      setTodos((todos) => [...todos, newTodo]);
+      // todos.push(newTodo);
+      // console.log(todos);
     }
-  }
+  };
 
   return (
     <div className="mt-5 container">
       <div className="card">
         <div className="card-header">Your Todo's</div>
         <div className="card-body">
-         <TodoTable todos={todos}/>
-         <button className='btn btn-primary' onClick={addTOdo}>
-          Add new button</button>
+          <TodoTable todos={todos} />
+          <button className="btn btn-primary" onClick={addTOdo}>
+            Add new button
+          </button>
         </div>
       </div>
     </div>
